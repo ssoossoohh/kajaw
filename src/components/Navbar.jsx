753703f2c00575d9497c1443bdd1475091564b3e
MMesaricM <mm2718@student.uni-lj.si>
2024-10-15 17:14:45 +0200

    Added font, changed text sizes and padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
-import React, { useRef, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const navigateToAbout = () => {
     navigate("/about");
   };
+
+  const navigateToWork = () => {
+    navigate("/");
+  };
+
+  const isAboutActive = location.pathname === "/about";
+  const isWorkActive = location.pathname === "/";
+
   return (
     <div className="home-title">
-      <div className="home-title-left">kaja m</div>
+      <div className="home-title-left">kaja mesaric</div>
       <div className="home-title-right">
-        <p className="htr-text">work</p>
-        <p className="htr-text" onClick={navigateToAbout}>
+        <p
+          className={`htr-text ${isWorkActive ? "active" : ""}`}
+          onClick={navigateToWork}
+        >
+          work
+        </p>
+        <p
+          className={`htr-text ${isAboutActive ? "active" : ""}`}
+          onClick={navigateToAbout}
+        >
           about
         </p>
       </div>
