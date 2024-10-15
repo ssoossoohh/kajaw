7147b28100c581d26e021e804c28bb248fee8936
MMesaricM <mm2718@student.uni-lj.si>
2024-10-15 15:49:31 +0200

    fixed route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export default function Navbar() {
   const navigate = useNavigate();
   const navigateToAbout = () => {
-    navigate("/about");
+    navigate("/kajaw/about");
   };
   return (
     <div className="home-title">
